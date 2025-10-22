1d575adf55e5fccc7c75a962b1d0ff1729e1132f
s22a0058-ai <s22a0058@siswa.umk.edu.my>
2025-10-22 16:50:56 +0800

    Update studentsurvey.py

--- studentsurvey.py
+++ studentsurvey.py
@@ -32,6 +32,13 @@ def load_data():
         st.error(f"Error loading data from URL: {e}")
         return pd.DataFrame()
 
+col1, col2, col3, col4 = st.columns(4)
+    
+col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
+col2.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
+col3.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
+col4.metric(label="PLO 5", value=f"4.3", help="PLO 5: Communication Skill", border=True)
+
 df = load_data()
 
 # Check if data loaded successfully
